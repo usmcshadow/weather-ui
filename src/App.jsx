7b640de517b10fef2a37992d470e6a69293a53b6
usmcshadow <shadowboyatl@gmail.com>
2026-02-02 14:59:27 -0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-50 flex items-center justify-center">
+      <h1 className="text-xl font-semibold">Weather UI</h1>
+      <h1 className="text-3xl font-bold text-brand">Tailwind OK</h1>
+    </div>
+  );
+}
